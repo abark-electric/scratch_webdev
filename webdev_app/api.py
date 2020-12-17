ec50face9fa0e6aeb0c69a06342a09d294ee3fa3
Your Name <code.abrarzawad@gmail.com>
2020-12-17 01:17:46 -0500

    Created serializer and api files

---
+++ webdev_app/api.py
@@ -0,0 +1,14 @@
+from webdev_app.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+
+# Lead Viewset
+# Creates full CRUD API without having to specify explicit methods for the functionality
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ***REMOVED***
+    serializer_class = LeadSerializer
