19705d7a8fc9ec696e758cc5ec22a4225b8d95a9
Your Name <code.abrarzawad@gmail.com>
2020-12-19 01:47:36 -0500

    Added more files

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Components {
+render() {
+return <h1>React App</h1.
+}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
